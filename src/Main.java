9d42dbaaa668ee7dc76c24f7ddbc30ab37f9dd5a
Evgeny Leshevich <evgeniy.le99@gmail.com>
2021-08-17 13:50:49 +0300

    Add new List object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
+
+        List<User> users = new ArrayList<>();
+
+        User userEvgeniy = new User(22,"Evgeniy");
+        User userKate = new User(20,"Kate");
+        users.add(userEvgeniy);
+        users.add(userKate);
+
+        for (User u :
+                users) {
+            System.out.println("My name is " + u.getFirstName() + ". I'm " + u.getAge() + " years old.");
+        }
     }
 }
